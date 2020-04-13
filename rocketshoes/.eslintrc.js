ccfdbf328f4c2f962d25347e79fe2436ac8174c9
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-04-12 21:41:22 -0300

    Redux

--- rocketshoes/.eslintrc.js
+++ rocketshoes/.eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
   env: {
-    browser: true,
     es6: true,
   },
   extends: [
-    'plugin:react/recommended',
     'airbnb',
+    'prettier',
+    'prettier/react'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly'
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -20,14 +22,22 @@ module.exports = {
   },
   plugins: [
     'react',
+    'prettier'
   ],
   rules: {
-    'import/prefer-default-export': 'off',
+    'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
       'warn',
       {
         extensions: ['.jsx', '.js']
       }
     ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off'
   },
 };
--- rocketshoes/src/App.js
+++ rocketshoes/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyles from './styles/global';
 import Header from './components/Header/index';
 import Routes from './routes';
 
+import store from './store';
+
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Routes />
-        <GlobalStyles />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Routes />
+          <GlobalStyles />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
