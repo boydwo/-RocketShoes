0d3f77d5e30092fa551d8dad46ea1ea786ab8245
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-04-14 09:20:17 -0300

    redux_update_amount

--- rocketshoes/src/pages/Cart/index.js
+++ rocketshoes/src/pages/Cart/index.js
@@ -11,7 +11,13 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart }) {
+function Cart({ cart, removeFromCart, updateAmount }) {
+  function increment(product) {
+    updateAmount(product.id, product.amount + 1);
+  }
+  function decrement(product) {
+    updateAmount(product.id, product.amount - 1);
+  }
   return (
     <Container>
       <ProductTable>
@@ -36,11 +42,11 @@ function Cart({ cart, removeFromCart }) {
               </td>
               <td>
                 <div>
-                  <button type="button">
+                  <button type="button" onClick={() => decrement(product)}>
                     <MdRemoveCircleOutline size={20} color="#7159c1" />
                   </button>
                   <input type="number" readOnly value={product.amount} />
-                  <button type="button">
+                  <button type="button" onClick={() => increment(product)}>
                     <MdAddCircleOutline size={20} color="#7159c1" />
                   </button>
                 </div>
@@ -67,7 +73,7 @@ function Cart({ cart, removeFromCart }) {
           <strong>R$1920,28</strong>
         </Total>
       </footer>
-    </Container >
+    </Container>
   );
 }
 const mapStateToProps = (state) => ({
